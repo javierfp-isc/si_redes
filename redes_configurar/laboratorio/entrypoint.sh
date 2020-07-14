3ed4f244d8bd46a68e190669de49715c12ad745a
Javier Fernández Peón <javierfp@iessanclemente.net>
2020-07-14 19:35:28 +0200

    Configurar red

---
+++ redes_configurar/laboratorio/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Borramos la configuración previa de la red de docker
+ip addr flush eth0
+
+#Recargamos la configuración de red utilizando el paquete ifupdown
+service networking restart
+
+#Uso exec para lanzar un proceso independiente de bucle infinito
+exec bash -c "while true;do sleep 10;done"
+
